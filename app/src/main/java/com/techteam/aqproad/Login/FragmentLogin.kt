dee592d7fb62edfed60760a03f0d6601b261c3ef
amoroccoire <amoroccoire@unsa.edu.pe>
2024-11-13 19:38:37 -0500

    fragment de login, gestiona la autenticacion

--- app/src/main/java/com/techteam/aqproad/Login/FragmentLogin.kt
+++ app/src/main/java/com/techteam/aqproad/Login/FragmentLogin.kt
@@ -1,60 +1,81 @@
 package com.techteam.aqproad.Login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.techteam.aqproad.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentLogin.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentLogin : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        auth = (activity as LoginActivity).auth
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentLogin.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentLogin().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val txtRegister = view.findViewById<TextView>(R.id.txtRegister)
+        txtRegister.setOnClickListener{
+            val fragmetRegister = FragmentRegister()
+
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainer, fragmetRegister)
+                .addToBackStack(null)
+                .commit()
+        }
+        setUp(view)
+    }
+
+    private fun setUp(view: View) {
+        val btnLoginNormal = view.findViewById<Button>(R.id.btnLoginNormal)
+        btnLoginNormal.setOnClickListener{
+            val user = view.findViewById<EditText>(R.id.inputUserLogin)
+            val pass = view.findViewById<EditText>(R.id.inputPasswordLogin)
+
+            if (user.text.isNotEmpty() && pass.text.isNotEmpty()) {
+                Toast.makeText(
+                    view.context,
+                    "Llena todos los campos",
+                    Toast.LENGTH_SHORT,
+                ).show()
             }
+            else {
+
+                val email = user.text.toString()
+                val password = user.text.toString()
+
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("LOGIN", "SIGNIN Exitoso")
+                            val user = auth.currentUser
+                            Log.d("LOGIN", user.toString())
+                        } else {
+                            Log.w("LOGIN", "SIGNING FALLO", task.exception)
+                            Toast.makeText(
+                                view.context,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
+            }
+        }
     }
 }
