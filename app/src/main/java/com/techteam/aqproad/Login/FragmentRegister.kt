86340f739df356923e6f839dd76c81c117ff8b79
amoroccoire <amoroccoire@unsa.edu.pe>
2024-11-13 19:39:25 -0500

    se cambio el nombre

--- app/src/main/java/com/techteam/aqproad/Login/FragmentRegister.kt
+++ app/src/main/java/com/techteam/aqproad/Login/FragmentRegister.kt
@@ -1,33 +1,24 @@
 package com.techteam.aqproad.Login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.techteam.aqproad.R
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [fragment_register.newInstance] factory method to
- * create an instance of this fragment.
- */
-class fragment_register : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class FragmentRegister : Fragment() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        auth = (activity as LoginActivity).auth
     }
 
     override fun onCreateView(
@@ -38,23 +29,50 @@ class fragment_register : Fragment() {
         return inflater.inflate(R.layout.fragment_register, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_register.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            fragment_register().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val txtLogin = view.findViewById<TextView>(R.id.txtLogin)
+        txtLogin.setOnClickListener {
+            parentFragmentManager.popBackStack()
+        }
+
+        setUp(view)
+    }
+
+    private fun setUp(view: View) {
+        val btnContinue = view.findViewById<Button>(R.id.btnContinue)
+        btnContinue.setOnClickListener{
+            val name = view.findViewById<EditText>(R.id.inputName)
+            val mail = view.findViewById<EditText>(R.id.inputMail)
+            val user = view.findViewById<EditText>(R.id.inputUser)
+            val pass1 = view.findViewById<EditText>(R.id.inputPassword)
+            val pass2 = view.findViewById<EditText>(R.id.inputVerifyPassword)
+
+            if (name.text.isNotEmpty() && mail.text.isNotEmpty() && user.text.isNotEmpty() && pass1.text.isNotEmpty() && pass2.text.isNotEmpty() && (pass1.text.toString() == pass2.text.toString())) {
+                val email = mail.text.toString()
+                val password = mail.text.toString()
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Log.d("REGISTER", "Usuario creado satisfactoriamnete")
+                            val user = auth.currentUser
+                            Log.d("TAG", user.toString())
+                            parentFragmentManager.popBackStack()
+                        }
+                        else {
+                            Log.w("REGISTER", "Fallo al crear el usuario", task.exception)
+                            Toast.makeText(
+                                view.context,
+                                "Authentication failed.",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                        }
+                    }
             }
+            else {
+                Toast.makeText(view.context, "Vuelva a intentarlo", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
