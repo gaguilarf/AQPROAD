7f17d5be11ec0e8991eeb47d48aa4d9f480f32c8
Rodrigo Véliz <rvelizs@unsa.edu.pe>
2024-11-15 01:08:56 -0500

    Calificaciones con restricciones

--- app/src/main/java/com/techteam/aqproad/Item/ItemFragment.kt
+++ app/src/main/java/com/techteam/aqproad/Item/ItemFragment.kt
@@ -1,23 +1,53 @@
 package com.techteam.aqproad.Item
 
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.techteam.aqproad.R
+import com.bumptech.glide.Glide
 
 class ItemFragment : Fragment() {
+    private lateinit var fusedLocationClient:FusedLocationProviderClient
+    private lateinit var view: View
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_item, container, false)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        view = inflater.inflate(R.layout.fragment_item, container, false)
 
         val btnExpand: ImageButton = view.findViewById(R.id.btn_expand)
+        val ratingBar: RatingBar = view.findViewById(R.id.ratingCalif)
+        val imagenprueba: ImageView = view.findViewById(R.id.imagenPrueba)
+
+        Glide.with(view.context)
+            .load("https://d3cjd3eir1atrn.cloudfront.net/jesusCautivo.jpg")
+            .into(imagenprueba)
 
         btnExpand.setOnClickListener {
             val fragment = CroquisFragment()
@@ -27,6 +57,105 @@ class ItemFragment : Fragment() {
             transaction.commit()
         }
 
+        ratingBar.setOnRatingBarChangeListener { _, rating, _ ->
+            // Manejar el valor de la calificación
+            val permite = isUserLoggedIn() // si está registrado
+            if (permite) {
+                isUserAtLocation { isAtLocation ->
+                    if (!isAtLocation) {
+                        showToast("Debe estar en la ubicación de la edificación para calificar.", view)
+                        ratingBar.rating = 0f
+                        return@isUserAtLocation
+                    } else {
+                        // El usuario está en la ubicación, puedes proceder
+                        showToast("Puedes calificar.", view)
+                    }
+                }
+            } else {
+                showToast("Debe iniciar sesión para calificar.", view)
+                ratingBar.rating = 0f
+                return@setOnRatingBarChangeListener
+            }
+            /*
+            if (!isUserLoggedIn()) {
+                showToast("Debe iniciar sesión para calificar.", view)
+                ratingBar.rating = 0f
+                return@setOnRatingBarChangeListener
+            }
+            isUserAtLocation { isAtLocation ->
+                if (permite && !isAtLocation) {
+                    showToast("Debe estar en la ubicación de la edificación para calificar.", view)
+                    ratingBar.rating = 0f
+                    return@isUserAtLocation
+                } else {
+                    // El usuario está en la ubicación, puedes proceder
+                    showToast("Puedes calificar.", view)
+                }
+            }*/
+            saveUserRating(rating, view)
+        }
+
         return view
+    }
+
+
+
+    private fun saveUserRating(rating: Float, view: View) {
+        showToast("Gracias por calificar con $rating estrellas.", view)
+        // Guardar la calificación en la base de datos o realizar otra acción
+    }
+    private fun showToast(message: String, view: View) {
+        Toast.makeText(view.context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun isUserLoggedIn(): Boolean {
+        // Verificar si hay un usuario logeado, por ejemplo, usando SharedPreferences o tu sistema de autenticación.
+        //val sharedPreferences = getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        //return sharedPreferences.getBoolean("is_logged_in", false)
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser != null) {
+            return true
+        }
+        return false
+    }
+
+    private fun isUserAtLocation(onResult: (Boolean) -> Unit) {
+        // Coordenadas del lugar de la edificación
+        val targetLatitude = -16.3911262
+        val targetLongitude = -71.5122978
+
+        // Verificar permisos de ubicación
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Si no se tienen permisos, puedes solicitar los permisos aquí
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE)
+            showToast("No tiene permisos", view)
+            onResult(false)
+            return
+        }
+
+        // Obtener la ubicación actual
+        fusedLocationClient.lastLocation.addOnSuccessListener(requireActivity(), OnSuccessListener<Location> { location ->
+            if (location != null) {
+                val userLatitude = location.latitude
+                val userLongitude = location.longitude
+
+                // Verificar si el usuario está en la ubicación deseada
+                val distance = FloatArray(1)
+                Location.distanceBetween(userLatitude, userLongitude, targetLatitude, targetLongitude, distance)
+
+                // Considera un rango de tolerancia (ejemplo: 50 metros)
+                val tolerance = 60.0f
+                showToast("Sí lee ubi", view)
+                Log.d("TAGAAAA", distance[0].toString())
+                onResult(distance[0] <= tolerance)
+            } else {
+                // Si no se puede obtener la ubicación
+                showToast("No puede obtener la ubicación", view)
+                onResult(false)
+            }
+        })
     }
 }
