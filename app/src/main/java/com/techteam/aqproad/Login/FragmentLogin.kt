1dcfc8bf4bbf3c2451f28561595ce4ba0a51274b
amoroccoire <amoroccoire@unsa.edu.pe>
2024-11-15 15:01:22 -0500

    se agrego la funcion que permite setear al objeto currentUser el nombre de usuario, la funcion de busqueda por email sera usada cuando se use google o facebook y se modularizado el codigo

--- app/src/main/java/com/techteam/aqproad/Login/FragmentLogin.kt
+++ app/src/main/java/com/techteam/aqproad/Login/FragmentLogin.kt
@@ -12,12 +12,17 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
 import com.techteam.aqproad.MainActivity
 import com.techteam.aqproad.R
 
 class FragmentLogin : Fragment() {
 
     private lateinit var auth: FirebaseAuth
+    private lateinit var viewGlobal: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         auth = (activity as LoginActivity).auth
@@ -32,6 +37,7 @@ class FragmentLogin : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        viewGlobal = view
         val txtRegister = view.findViewById<TextView>(R.id.txtRegister)
         txtRegister.setOnClickListener{
             val fragmetRegister = FragmentRegister()
@@ -47,39 +53,106 @@ class FragmentLogin : Fragment() {
     private fun setUp(view: View) {
         val btnLoginNormal = view.findViewById<Button>(R.id.btnLoginNormal)
         btnLoginNormal.setOnClickListener{
-            val user = view.findViewById<EditText>(R.id.inputUserLogin)
+            val userName = view.findViewById<EditText>(R.id.inputUserLogin)
             val pass = view.findViewById<EditText>(R.id.inputPasswordLogin)
 
-            if (user.text.isNotEmpty() && pass.text.isNotEmpty()) {
-                val email = user.text.toString()
-                val password = user.text.toString()
+            if (fieldsNotEmpty(userName, pass)) {
+                val username = userName.text.toString()
+                val password = pass.text.toString()
 
-                auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
-                        if (task.isSuccessful) {
-                            Log.d("LOGIN", "SIGNIN Exitoso")
-                            val user = auth.currentUser
-                            Log.d("LOGIN", user.toString())
-                            val intent = Intent(requireContext(), MainActivity::class.java)
-                            startActivity(intent)
-                            requireActivity().finish()
-                        } else {
-                            Log.w("LOGIN", "SIGNING FALLO", task.exception)
-                            Toast.makeText(
-                                view.context,
-                                "Authentication failed.",
-                                Toast.LENGTH_SHORT,
-                            ).show()
-                        }
-                    }
+                signInWithUsername(username, password)
             }
             else {
-                Toast.makeText(
-                    view.context,
-                    "Llena todos los campos",
-                    Toast.LENGTH_SHORT,
-                ).show()
+                Toast.makeText(view.context, "Llena todos los campos", Toast.LENGTH_SHORT).show()
             }
         }
     }
+
+    private fun fieldsNotEmpty(vararg fields: EditText): Boolean{
+        return fields.all { it.text.isNotEmpty() }
+    }
+
+    private fun signInWithUsername(username: String, password: String) {
+
+        getEmailByUsername(username,
+            onSuccess = {email ->
+                signInWithEmail(email, password,
+                    onSuccess = { user ->
+                        Log.d("LOGIN", "SIGNIN Exitoso")
+                        updatePerfil(user, username)
+                        val intent = Intent(requireContext(), MainActivity::class.java)
+                        startActivity(intent)
+                        requireActivity().finish()
+                    },
+                    onFailure = { error ->
+                        Toast.makeText(viewGlobal.context, error, Toast.LENGTH_SHORT).show()
+                    }
+                )
+            },
+            onFailure = { error ->
+                Toast.makeText(viewGlobal.context, error, Toast.LENGTH_SHORT).show()
+            }
+        )
+
+    }
+
+    private fun getEmailByUsername(
+        username: String,
+        onSuccess: (String) -> Unit,
+        onFailure: (String) -> Unit
+    ) {
+        val db = FirebaseFirestore.getInstance()
+        db.collection("Usuario").document(username).get()
+            .addOnSuccessListener {document ->
+                if (document.exists() && document.contains("email")) {
+                    val email = document.getString("email")
+                    if (email != null) {
+                        onSuccess(email)
+                    }
+                    else {
+                        onFailure("El correo no se encontró en el documento")
+                    }
+                }
+            }
+            .addOnFailureListener {
+                onFailure("Error al consultar Firestore: ${it.message}")
+            }
+    }
+
+    private fun signInWithEmail(
+        email: String,
+        password: String,
+        onSuccess: (FirebaseUser) -> Unit,
+        onFailure: (String) -> Unit
+    ) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    auth.currentUser?.let {
+                        it -> onSuccess(it)
+                    }
+                } else {
+                    onFailure(task.exception?.message ?: "Error desconocido al iniciar sesión")
+                }
+            }
+    }
+
+    private fun updatePerfil(user: FirebaseUser, username: String){
+        val profileUpdates = UserProfileChangeRequest.Builder()
+            .setDisplayName(username)
+            .build()
+
+        user.updateProfile(profileUpdates)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("FRAGMENT LOGIN", "Se ha seteado el username al objeto currentUser")
+                    Toast.makeText(context, "Perfil actualizado", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    Log.d("FRAGMENT LOGIN", "Error al guardar el username al objeto currentUser")
+                    Toast.makeText(context, "Error al actualizar perfil", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
