d84969105b1385f8ff26323b05279c3d62597b17
amoroccoire <amoroccoire@unsa.edu.pe>
2024-11-15 14:58:43 -0500

    agregando guardado de usuario en firebase database al registrarse, si el usuario escribe un correo que ya existe firebase lo rechazara

--- app/src/main/java/com/techteam/aqproad/Login/FragmentRegister.kt
+++ app/src/main/java/com/techteam/aqproad/Login/FragmentRegister.kt
@@ -12,8 +12,10 @@ import android.widget.TextView
 import com.techteam.aqproad.R
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class FragmentRegister : Fragment() {
+    private val db = FirebaseFirestore.getInstance()
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,18 +47,27 @@ class FragmentRegister : Fragment() {
         btnContinue.setOnClickListener{
             val name = view.findViewById<EditText>(R.id.inputName)
             val mail = view.findViewById<EditText>(R.id.inputMail)
-            val user = view.findViewById<EditText>(R.id.inputUser)
+            val username = view.findViewById<EditText>(R.id.inputUser)
             val pass1 = view.findViewById<EditText>(R.id.inputPassword)
             val pass2 = view.findViewById<EditText>(R.id.inputVerifyPassword)
 
-            if (name.text.isNotEmpty() && mail.text.isNotEmpty() && user.text.isNotEmpty() && pass1.text.isNotEmpty() && pass2.text.isNotEmpty() && (pass1.text.toString() == pass2.text.toString())) {
+            if (name.text.isNotEmpty() && mail.text.isNotEmpty() && username.text.isNotEmpty() && pass1.text.isNotEmpty() && pass2.text.isNotEmpty() && (pass1.text.toString() == pass2.text.toString())) {
                 val email = mail.text.toString()
-                val password = mail.text.toString()
+                val password = pass1.text.toString()
+                Log.d("TAAAAAAAAAAAAAAAAAAAG", "EMAIL: " + email + "\nPASS: " + password)
                 auth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
                             Log.d("REGISTER", "Usuario creado satisfactoriamnete")
                             val user = auth.currentUser
+                            saveNewUser(name.text.toString(), mail.text.toString(), username.text.toString()) { sucess, error ->
+                                if (sucess) {
+                                    Toast.makeText(view.context, "Usuario registrado exitosamente", Toast.LENGTH_SHORT).show()
+                                }
+                                else {
+                                    Toast.makeText(view.context, "Error al guardar usuario: $error", Toast.LENGTH_SHORT).show()
+                                }
+                            }
                             Log.d("TAG", user.toString())
                             parentFragmentManager.popBackStack()
                         }
@@ -73,6 +84,25 @@ class FragmentRegister : Fragment() {
             else {
                 Toast.makeText(view.context, "Vuelva a intentarlo", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    private fun saveNewUser(
+        nombreReal: String,
+        correo: String,
+        nombreUsuario: String,
+        callback: (Boolean, String?) -> Unit
+    ) {
+        db.collection("Usuario").document(nombreUsuario).set(
+            hashMapOf(
+                "email" to correo,
+                "name" to nombreReal,
+                "photoUrl" to ""
+            )
+        ).addOnSuccessListener {
+            callback(true, null) //exito
+        }.addOnFailureListener {exception ->
+            callback(false, exception.message) //error pipipi
         }
     }
 }
