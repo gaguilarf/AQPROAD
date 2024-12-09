a1c3e90c1a41a0ae767f04bb677115977cc84860
bryanhancco <bhanccoco@unsa.edu.pe>
2024-12-08 19:56:03 -0500

    Reorganizacion de imagenes

--- app/src/main/java/com/techteam/aqproad/Item/ItemFragment.kt
+++ app/src/main/java/com/techteam/aqproad/Item/ItemFragment.kt
@@ -74,7 +74,6 @@ class ItemFragment : Fragment() {
             val fragment = ItemFragment()
             val args = Bundle() // creando bundle
             args.putInt(ARG_BUILDING_ID, buildingId) //agregando id de la edificacion al bundle
-            //args.putString(ARG_BUILDING_NAME, buildingId)
             fragment.arguments = args //asignando el bundle al fragment
             return fragment
         }
@@ -84,7 +83,6 @@ class ItemFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
         buildingID = arguments?.getInt(ARG_BUILDING_ID)?:0//recupera el id de la edificacion pasada
-        //buildingName = arguments?.getString(ARG_BUILDING_NAME)
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
         ratingManagerDB = RatingManagerDB()
@@ -98,91 +96,6 @@ class ItemFragment : Fragment() {
         setupUI(view)
         setupObservers()
         return view
-
-        // Referencias a los elementos UI
-        /*val btnExpand: ImageButton = view.findViewById(R.id.btn_expand)
-        val ratingBar: RatingBar = view.findViewById(R.id.ratingCalif)
-        editTextTextMultiLine = view.findViewById<EditText>(R.id.editTextTextMultiLine)  // EditText para comentarios
-
-        // Arranque del carrusel
-        setupCarouselRecyclerView()
-
-        // Configuración del RecyclerView para comentarios
-        recyView_comentarios = view.findViewById(R.id.recyView_comentarios)
-        recyView_comentarios.layoutManager = LinearLayoutManager(requireContext())
-
-        val repository = ComentarioRepository()
-        viewModel_comentarios = ComentarioViewModel(repository)
-        viewModel_comentarios.comentarios.observe(viewLifecycleOwner, Observer { comentarios ->
-            adapter_comentarios = ComentarioAdapter(comentarios)
-            recyView_comentarios.adapter = adapter_comentarios
-        })
-
-        viewModel_comentarios.loadComentarios()
-
-        // Lógica para el botón de expansión (abre otro fragmento)
-        btnExpand.setOnClickListener {
-            val fragment = CroquisFragment()
-            val transaction: FragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
-            transaction.replace(R.id.main_container, fragment)
-            transaction.addToBackStack(null)
-            transaction.commit()
-        }
-
-        // Lógica para el botón de enviar comentario
-        val btnSendComment: Button = view.findViewById<Button>(R.id.button_send_comment)
-        btnSendComment.setOnClickListener {
-            val commentText = editTextTextMultiLine.text.toString().trim()
-
-            if (commentText.isNotEmpty()) {
-                val usuarioActual = FirebaseAuth.getInstance().currentUser
-                val userName = usuarioActual?.displayName ?: "Usuario"
-                val newComment = Comentario(
-                    id = (viewModel_comentarios.comentarios.value?.size ?: 0) + 1,  // Generar un id secuencial para el comentario
-                    autor = userName.toString(),
-                    contenido = commentText
-                )
-
-                // Agregar el nuevo comentario a la lista
-                viewModel_comentarios.addComentario(newComment)
-
-                // Limpiar el campo de texto después de enviar el comentario
-                editTextTextMultiLine.text.clear()
-
-                // Mostrar un mensaje de éxito
-                showToast("Comentario enviado", view)
-            } else {
-                showToast("Por favor escribe un comentario", view)
-            }
-        }
-
-        // Configuración de la RatingBar y permisos para calificar
-        var isResettingRating = false
-        ratingBar.setOnRatingBarChangeListener { _, rating, _ ->
-            if (isResettingRating) return@setOnRatingBarChangeListener
-
-            if (isUserLoggedIn()) {
-                isUserAtLocation { isAtLocation ->
-                    if (!isAtLocation) {
-                        showToast("Debe estar en la ubicación de la edificación para calificar.", view)
-                        isResettingRating = true
-                        ratingBar.rating = 0f
-                        isResettingRating = false
-                        return@isUserAtLocation
-                    } else {
-                        showToast("Puedes calificar.", view)
-                        saveUserRating(rating, view)
-                    }
-                }
-            } else {
-                showToast("Debe iniciar sesión para calificar.", view)
-                isResettingRating = true
-                ratingBar.rating = 0f
-                isResettingRating = false
-                return@setOnRatingBarChangeListener
-            }
-        }*/
-
     }
 
     private fun setupUI(view: View) {
